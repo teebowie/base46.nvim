d14b573dae6a68089c34851079d6585caaa4c741
Teebowie <69783484+teebowie@users.noreply.github.com>
2022-09-07 15:34:35 +0700

    Update base46_hls.lua

--- base46_hls.lua
+++ base46_hls.lua
@@ -1,17 +1,17 @@
-local merge_tbl = require("teebowie.plugins.base46").merge_tbl
+local merge_tbl = require("base46").merge_tbl
 
 local M = {}
 
 function M.get_hls(colors, transparent)
     local highlights = {}
-    local integrations = require("teebowie.plugins.base46.integrations")
+    local integrations = require("base46.integrations")
 
     if transparent then
         table.insert(integrations, "glassy")
     end
 
     for _, integration_module in ipairs(integrations) do
-        local hls = require("teebowie.plugins.base46.integrations." .. integration_module)(colors)
+        local hls = require("base46.integrations." .. integration_module)(colors)
         highlights = merge_tbl(highlights, hls)
     end
 
